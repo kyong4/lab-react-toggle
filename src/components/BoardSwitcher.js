4627cc11fbf613372429526f788ff537cb972e74
Kevin Yong <euler004@gmail.com>
2022-09-20 22:51:33 -0400

    lab2

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react"; //
 
 function Board(props) {
   let className = "board";
@@ -9,17 +10,33 @@ function Board(props) {
   return <div className={className}>{props.index + 1}</div>;
 }
 
+
 function BoardSwitcher(props) {
   let boards = [];
+//start
+  const [selected, setSelected] = useState(0);
+
+const incrementClick = () => {
+  setSelected((prevSelected)=> {
+    let nextSelected = prevSelected+1;
+    if (nextSelected>4) {
+      nextSelected=0;
+    }
+    return nextSelected;
+  });
+};
+//end
+
+
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selected;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={incrementClick}> Toggle</button>
     </div>
   );
 }
